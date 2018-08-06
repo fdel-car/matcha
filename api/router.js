d3230e88bc6b874bae80d47beaddfd33b1daa455
fdel-car <fdel-car@student.42.fr>
2018-08-06 21:52:24 +0200

    console.error on fallback

--- api/router.js
+++ api/router.js
@@ -4,7 +4,8 @@ const db = require('../db/index');
 const bcrypt = require('bcrypt');
 
 router.post('/users', (req, res) => {
-  if (!req.body) return res.status(400).json({ error: 'No body passed to make the call.' })
+  if (!req.body)
+    return res.status(400).json({ error: 'No body passed to make the call.' });
   const { username, first_name, last_name, email, password } = req.body;
   bcrypt.hash(password, 12, async function(err, hash) {
     if (err) throw err;
@@ -13,7 +14,9 @@ router.post('/users', (req, res) => {
         'INSERT INTO schema.users (id, username, first_name, last_name, email, password) VALUES (DEFAULT, $1, $2, $3, $4, $5)',
         [username, first_name, last_name, email, hash]
       );
-      res.status(200).send(`Hi ${username} 👋, you're successfully registered!`);
+      res
+        .status(200)
+        .send(`Hi ${username} 👋, you're successfully registered!`);
     } catch (err) {
       // For now it only get the first error, I want them all
       if (err.code == 23505) {
@@ -21,6 +24,7 @@ router.post('/users', (req, res) => {
         error = `This ${fieldName} is already taken.`;
         return res.status(400).json({ error, fieldName });
       }
+      console.error(err.message);
       res.sendStatus(500);
     }
   });
